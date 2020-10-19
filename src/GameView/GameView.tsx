d8855ab856b6d9b9589fd69cb89015c373c52149
Jordan Mackie <jmackie97@hotmail.com>
2020-10-19 22:43:01 +0100

    I GET REACT.FC NOW

--- src/GameView/GameView.tsx
+++ src/GameView/GameView.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
-type FaceContainerProps = {
-    image: String,
-    name: String,
+interface FaceContainerProps {
+    image: string,
+    name: string,
 }
 
-const FacesGrid = (faces: FaceContainerProps[]) => {
-    return faces.map((face, idx) =>
+const FacesGrid: React.FC<{ faces: FaceContainerProps[] }> = ({faces}) => {
+    const faceGrid = faces.map((face, idx) =>
         <div key={idx}>
             <FaceContainer image={face.image} name={face.name}/>
         </div>
     );
+    return <div>{faceGrid}</div>;
 }
 
 const FaceContainer = ({image, name}: FaceContainerProps) => {
@@ -20,9 +21,23 @@ const FaceContainer = ({image, name}: FaceContainerProps) => {
     </div>
 }
 
-const GameView = () => {
-    return <div>
+function randomString(length: number) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
 
+const generateFaces = (): Array<FaceContainerProps> => {
+    let faces: Array<FaceContainerProps> = []
+    let numFaces = 3 * 8;
+    for (let i = 0; i < numFaces; i++) {
+        faces.push({name: randomString(7), image: randomString(7)})
+    }
+    return faces;
+}
+
+const GameView = () => {
+    let faces: Array<FaceContainerProps> = generateFaces();
+    return <div>
+        <FacesGrid faces={faces}/>
     </div>
 }
 
